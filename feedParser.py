c4d8c382826045216d703418b468f84abfba7428
DevSunshine01 <53397088+DevSunshine01@users.noreply.github.com>
2022-01-20 00:54:27 -0800

    Conversion display successful
    Removed old research print and RSS info requests to a getInputData function.
    
    Enabled a loop to search more than once for a user.
    
    Nailed down index-based RSS feed tag data for the description and itemList in findConversion(..) to accurately display the conversion countries, codes, and values.
    
    Added an import time - will be used to automate a time-based refresh of the user's chosen currency.

--- feedParser.py
+++ feedParser.py
@@ -1,9 +1,9 @@
-import requests
 import os
 from bs4 import BeautifulSoup as bs
 import feedparser as fp
+import requests
 import certifi
-
+import time
 
 def getFeedURLByCountryCode(code):     
         rssLink = startURL + code[0:3]  + endURL
@@ -39,60 +39,42 @@ def getAllCountriesCodes():
 def findConversion(code1, code2, rssLink):
         countryList = getRSSLinkInfoByTag(rssLink, 'title')
         itemList = getRSSLinkInfoByTag(rssLink, 'item')
+        descriptionList = getRSSLinkInfoByTag(rssLink, 'description')
         conversionRate = 0.0
         item = 0
-        print(code2.upper())
-        print(code1.upper())
         for c in countryList:
                 ##print(c)
                 ##print(type(c))             
                 if code1.upper() in c.string:
                         print(c.string)
-                        print(itemList[item])
+                        ##print(itemList[item])
+                        print(descriptionList[item].string)
                 item= item +1
                                 
+def getInputData(rssLink):
+        CountryInfo = getRSSLinkInfoByTag(rssLink, 'item')
+        CurrencyInfo = getRSSLinkInfoByTag(rssLink, 'description')
+        TitleInfo = getRSSLinkInfoByTag(rssLink, 'title')                 
+        ##printRSSInfo(CountryInfo)
+        ##printRSSInfo(CurrencyInfo)
+        ##printRSSInfo(TitleInfo)
         
+alive = True
 url = 'https://www.fx-exchange.com/currency-exchange-rates-rss-feed.html'
 startURL = 'https://www.fx-exchange.com/'
 endURL = '/rss.xml'
 
-anchorInfo1 = input('Please enter a three letter country code to convert from: ')
-rssLink1 = getFeedURLByCountryCode(anchorInfo1.lower())
-##CountryInfo1 = getRSSLinkInfoByTag(rssLink1, 'item')
-##CurrencyInfo1 = getRSSLinkInfoByTag(rssLink1, 'description')
-##TitleInfo1 = getRSSLinkInfoByTag(rssLink1, 'title')
-##printRSSInfo(CurrencyInfo1)   
-
-anchorInfo2 = input('Please enter a three letter country code to convert to: ')
-rssLink2 = getFeedURLByCountryCode(anchorInfo2.lower())
-##CountryInfo2 = getRSSLinkInfoByTag(rssLink2, 'item')
-##CurrencyInfo2 = getRSSLinkInfoByTag(rssLink2, 'description')
-##TitleInfo2 = getRSSLinkInfoByTag(rssLink2, 'title')
-##printRSSInfo(CurrencyInfo2)
-findConversion(anchorInfo1, anchorInfo2, rssLink2);
-
-print("---------------------------------\n")
-print("---------------------------------\n")
-
-'''
-url2 = 'https://www.mccarran.com/Flights/Arrivals'
-req = requests.get(url2)
-soup = bs(req.content, 'html.parser')
-
-print(soup)
-##feeds = soup.find('a', {'title' : 'RSS Feed'}).find('a')
-ul = soup.find(class
-tags = soup.find_all('a').text
-
-##print(feeds)
- 
-print("---------------------------------\n")
-print(allURL)
-url2 = 'https://www.mccarran.com/Flights/Arrivals'
-req_url = urllib.request.urlopen(url2, context=ssl.create_default_context(cafile=certifi.where()))
-print(req_url)
-##orig_sslsocket_init = ssl.SSLSocket.__init__
-##ssl.SSLSocket.__init__ = lambda *args, cert_reqs=ssl.CERT_NONE, **kwargs: orig_sslsocket_init(*args, cert_reqs=ssl.CERT_NONE, **kwargs)
-
-print(requests.certs.where())
-'''
+while alive:        
+        anchorInfo1 = input('Please enter a three letter country code to convert from: ')
+        rssLink1 = getFeedURLByCountryCode(anchorInfo1.lower())
+        
+        anchorInfo2 = input('Please enter a three letter country code to convert to: ')
+        rssLink2 = getFeedURLByCountryCode(anchorInfo2.lower())
+        
+        findConversion(anchorInfo1, anchorInfo2, rssLink2);
+        print("---------------------------------\n")
+        if str(input('\nEnter 1 to continue')[0]) == "1":
+                continue
+        else:
+                alive = False
+        print("---------------------------------\n")
